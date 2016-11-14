19c1b7a89861f10cd692d8c779e00b47b95da4cc
Titus Fortner <titusfortner@gmail.com>
2016-11-14 10:08:08 +0000

    remove non-working spec

--- spec/watir_framework_spec.rb
+++ spec/watir_framework_spec.rb
@@ -1,10 +1,6 @@
 require "spec_helper"
 
 describe WatirFramework do
-  it "has a version number" do
-    expect(WatirFramework::VERSION).not_to be nil
-  end
-
   it "starts a driver" do
     browser = Watir::Browser.new
     expect(browser.window).to exist
