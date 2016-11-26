0600b6c5836536b3db6718a8818f495d1d850eb9
Titus Fortner <titusfortner@gmail.com>
2016-11-26 00:26:06 -0600

    reorganize directory structure

--- lib/watir_framework.rb
+++ lib/watir_framework.rb
@@ -3,6 +3,7 @@ require "watir_model"
 require 'watir_framework/pages/home'
 require 'watir_framework/pages/results'
 require 'watir_framework/models/search'
+require 'generators/generator'
 
 module WatirFramework
   # Your code goes here...
